f7cd31c3134eaba60c88104063b56706b71eb4e8
Mike Spiegel <mspiegel31@gmail.com>
2020-04-03 14:11:50 -0400

    decreasing notification size

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -46,6 +46,7 @@ beautiful.bg_normal         = "#141A1B"
 beautiful.bg_focus          = "#222B2E"
 beautiful.font              = "Noto Sans Regular 10"
 beautiful.notification_font = "Noto Sans Bold 14"
+beautiful.notification_icon_size=24
 beautiful.wallpaper = awful.util.get_configuration_dir() .. "night-sky.jpg"
 
 -- This is used later as the default terminal and editor to run.
