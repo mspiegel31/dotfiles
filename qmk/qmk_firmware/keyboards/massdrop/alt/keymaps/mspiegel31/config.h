af6cf9227ab85111f5be3a8dbdc7126ec6ab1fa1
mike <mspiegel31@gmail.com>
2021-04-22 20:41:13 -0400

    tweaking saturation

--- qmk/qmk_firmware/keyboards/massdrop/alt/keymaps/mspiegel31/config.h
+++ qmk/qmk_firmware/keyboards/massdrop/alt/keymaps/mspiegel31/config.h
@@ -1,5 +1,5 @@
 
 // RGB config, see https://beta.docs.qmk.fm/using-qmk/hardware-features/lighting/feature_rgb_matrix#rgb-matrix-effects-id-rgb-matrix-effects
 #define RGB_MATRIX_STARTUP_HUE 127 // cyan
-#define RGB_MATRIX_STARTUP_SAT 0 // white by default
+#define RGB_MATRIX_STARTUP_SAT 200 // white by default
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS //two tone, alphas one color, mods another
