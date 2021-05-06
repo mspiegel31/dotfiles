96c8c858bd062ad9b8aa322e2d4057f22aea5512
Mike Spiegel <mike@ripplescience.com>
2021-05-06 10:37:47 -0400

    moving play button

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Mute | Vol+ |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Mute | Vol+ | Play |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |  \   | Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TD(DBCBR),  TD(DBBRC)  ,    KC_MUTE, KC_VOLU, _______,
-    _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______,    KC_BSLS,    KC_MPRV, KC_VOLD, KC_MNXT
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TD(DBCBR),  TD(DBBRC),  KC_MUTE, KC_VOLU, KC_MPLY,
+    _______, _______, _______, _______, _______, _______, _______, _______,    KC_BSLS,    KC_MPRV, KC_VOLD, KC_MNXT
 ),
 
 /* Raise
