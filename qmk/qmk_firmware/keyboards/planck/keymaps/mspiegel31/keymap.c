c109af385ec504ccbe4dc07e2e4a894a136d9844
mike <mspiegel31@gmail.com>
2021-05-05 22:33:47 -0400

    more media key flashng

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -80,14 +80,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      | Prev | Vol- | Next |
+ * |      |      |      |      |      |             |      | Mute | Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE,    _______,    _______, KC_VOLU, _______,
-    _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______,    _______,    KC_MPRV, KC_VOLD, KC_MNXT
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,    _______,    _______, KC_VOLU, _______,
+    _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______,    KC_MUTE,    KC_MPRV, KC_VOLD, KC_MNXT
 ),
 
 /* Raise
