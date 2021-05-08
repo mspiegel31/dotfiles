b6a2e864500f04dc3bf7554e7e83816f8e121f47
mike <mspiegel31@gmail.com>
2021-05-07 22:27:22 -0400

    removing fn keys from raise layer

--- qmk/qmk_firmware/keyboards/planck/keymaps/niu/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/niu/keymap.c
@@ -88,8 +88,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_LPRN,  KC_RPRN, KC_EQL,  KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_LCBR,  KC_RCBR, KC_MINS, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_ASTR,  KC_LBRC,  KC_RBRC, KC_PGUP, KC_BSLS,
+    KC_DEL,  _______, _______, _______, _______, _______, _______, KC_UNDS,  KC_LCBR,  KC_RCBR, KC_MINS, KC_PIPE,
+    _______, _______, _______, _______, _______, _______, _______, KC_ASTR,  KC_LBRC,  KC_RBRC, KC_PGUP, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_HOME, KC_PGDN, KC_END
 ),
 
