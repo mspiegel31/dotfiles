595769038547e988c42bcb41afa99db7e5a2c728
mike <mspiegel31@gmail.com>
2021-05-08 09:47:48 -0400

    adding quotes under 'l' key

--- qmk/qmk_firmware/keyboards/planck/keymaps/niu/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/niu/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, FN,      KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_ENT,  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_LCTL, FN,      KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  FN,      KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR,  KC_PIPE,  _______, _______, KC_PIPE,
+    KC_DEL,  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR,  KC_PIPE,  KC_DQUO, _______, KC_PIPE,
     _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,  _______,  _______, KC_PGUP, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_HOME, KC_PGDN, KC_END
 ),
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______, KC_MUTE,
-    _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______, _______, KC_VOLU, KC_MPLY,
+    KC_DEL,  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______, KC_MUTE, KC_VOLU, KC_MPLY,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
 ),
 
