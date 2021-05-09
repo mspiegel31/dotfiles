e3627bbdb0dc69671e17d05313285e601dc499f4
mike <mspiegel31@gmail.com>
2021-05-09 15:52:31 -0400

    moving quote to to thumb; nav row on home row

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -70,21 +70,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  FN2  | Alt  | GUI  |Lower|    Space    |Raise |Enter | Left | Down |Right |
+ * | Ctrl |  FN2  | Alt  | GUI  |Lower|    Space    |Raise |   '  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------`
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT,  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |  [{  |  ]}  |  ()  | ' "  |      |
+ * | Del  |      |      |      |      |      |      |  [{  |  ]}  |  ()  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |   -  |   =  |  <   |  >   |      |  \|  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -92,10 +92,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,        KC_8,        KC_9,      KC_0,         KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, _______, TD(OBROBRK), TD(CBRCBRK), TD(DBPRN), TD(SQUODQUO), _______,
-    _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,      KC_LT,       KC_GT,     _______,      KC_BSLS,
-    _______, _______, _______, _______, _______, _______, _______, _______,     _______,     _______,   _______,      _______
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_DEL,  _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, _______,
+    _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LT,   KC_GT,   _______, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,  _______,  _______, KC_PGUP, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_HOME, KC_PGDN, KC_END
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN,  KC_BSPC,
+    KC_DEL,  _______, _______, _______, _______, _______, _______, KC_LEFT,  KC_UP,   KC_DOWN, KC_RIGHT, _______,
+    _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,  _______, _______, KC_PGUP,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_HOME, KC_PGDN,  KC_END
 ),
 
   /* FN2
