3788a5f5475a480d8f7631d62bdc9ad0b4ee9dad
Mike Spiegel <mspiegel31@gmail.com>
2021-05-09 19:47:29 -0400

    adding min double tap

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -49,6 +49,7 @@ enum {
   SQUODQUO,
   SEMI_EQ,
   SLASH_MIN,
+  COMMA_MIN,
   GRV_OR_TILD,
 };
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -60,6 +61,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [SQUODQUO] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),  // ' "
   [SEMI_EQ] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_EQL),    // ; =
   [SLASH_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_MINS), // / -
+  [COMMA_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_MINS),  // , -
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -76,10 +78,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------`
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,        KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    TD(SEMI_EQ), KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_UP,       TD(SLASH_MIN),
-    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT, KC_LEFT, KC_DOWN,     KC_RIGHT
+    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,          KC_O,    KC_P,        KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,          KC_L,    TD(SEMI_EQ), KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   TD(COMMA_MIN), KC_DOT,  KC_UP,       TD(SLASH_MIN),
+    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT,       KC_LEFT, KC_DOWN,     KC_RIGHT
 ),
 
 /* Lower
