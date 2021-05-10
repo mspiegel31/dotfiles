27ab8697e499eb779c7d45010a7d9598ac49a060
Mike Spiegel <mike@ripplescience.com>
2021-05-10 09:52:38 -0400

    removing slash double tap

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,          KC_O,    KC_P,        KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,          KC_L,    TD(SEMI_EQ), KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   TD(COMMA_MIN), KC_DOT,  KC_UP,       TD(SLASH_MIN),
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   TD(COMMA_MIN), KC_DOT,  KC_UP,       KC_SLSH,
     KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT,       KC_LEFT, KC_DOWN,     KC_RIGHT
 ),
 
