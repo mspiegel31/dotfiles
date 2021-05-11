6778fcd0781c1ed28c6492f7c61f5d4ea7a9e0bf
Mike Spiegel <mike@ripplescience.com>
2021-05-11 10:17:55 -0400

    expermienting with default layout

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -79,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,          KC_O,    KC_P,        KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,          KC_L,    TD(SEMI_EQ), KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   TD(COMMA_MIN), KC_DOT,  KC_UP,       KC_SLSH,
-    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_QUOT,       KC_LEFT, KC_DOWN,     KC_RIGHT
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,          KC_L,    TD(SEMI_EQ), KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   TD(COMMA_MIN), KC_DOT,  KC_SLSH,     KC_ENT,
+    KC_LCTL, FN2,     KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_LEFT,       KC_DOWN, KC_UP,       KC_RIGHT
 ),
 
 /* Lower
@@ -116,8 +116,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN,  KC_BSPC,
     _______, _______, _______, _______, _______, _______, _______, KC_MPRV,  KC_VOLU, KC_VOLD, KC_MNXT,  _______,
-    _______, _______, _______, _______, _______, _______, KC_UNDS, KC_MUTE,  KC_MPLY, _______, KC_BRIU,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_BRID,  _______
+    _______, _______, _______, _______, _______, _______, KC_UNDS, KC_MUTE,  KC_MPLY, _______, _______,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,  _______
 ),
 
   /* FN2
