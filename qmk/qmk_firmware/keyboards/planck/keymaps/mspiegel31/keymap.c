224774878ed1f1394d3c01e7a5b1e66046d6961d
Mike Spiegel <mike@ripplescience.com>
2021-05-11 20:01:34 -0400

    removing redundant symbols

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -98,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, _______,
-    _______, _______, _______, KC_MUTE, KC_MPLY, _______, KC_MINS, KC_EQL,  _______, _______, KC_BSLS, _______,
+    KC_DEL,  KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______,
+    _______, _______, _______, KC_MUTE, KC_MPLY, _______, _______, _______, _______, _______, KC_BSLS, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
 ),
 
