18fe3c46844ceac7c8e8e8b8006f7589f85c7f05
mike <mspiegel31@gmail.com>
2021-05-22 10:21:18 -0400

    using tab as alternate raise key

--- qmk/qmk_firmware/keyboards/preonic/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/preonic/keymaps/mspiegel31/keymap.c
@@ -39,7 +39,7 @@ enum preonic_keycodes {
 #define RAISE      MO(_RAISE)
 #define FN         MO(_FN)
 #define SPACE_FN   LT(_SPACE_FN, KC_SPC)
-#define LEFT_RAISE LT(_RAISE, KC_ESC)
+#define TAB_RAISE LT(_RAISE, KC_TAB)
 
 enum {
   DBCBR,
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,     KC_7,   KC_8,    KC_9,    KC_0,        KC_BSLS,
   KC_GRV,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,        KC_BSPC,
-  KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    TD(SEMI_EQ), KC_QUOT,
+  TAB_RAISE,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    TD(SEMI_EQ), KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH,     KC_SFTENT,
   KC_LCTL,    FN,      KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_LEFT, KC_DOWN, KC_UP,       KC_RIGHT
 ),
