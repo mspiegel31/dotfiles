98cb803550c749f3d022cbfe1dcf77ccad0ff95b
mike <mspiegel31@gmail.com>
2021-05-25 21:54:11 -0400

    removing space fn for now

--- qmk/qmk_firmware/keyboards/preonic/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/preonic/keymaps/mspiegel31/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,     KC_BSPC,
   TAB_RAISE,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    SEMI_EQ,  KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_SFTENT,
-  KC_LCTL,    FN,      KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN, RAISE,  KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT
+  KC_LCTL,    FN,      KC_LALT, KC_LGUI, LOWER,   KC_SPC,    KC_SPC, RAISE,  KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT
 ),
 
 /* Raise
@@ -120,7 +120,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |   =  |      |      | Prev | Vol- | Vol+ | Next |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |   -  |      |      | Play | Mute |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
