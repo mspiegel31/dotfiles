0ba286faabfacd2144330fab394699b61a8f5dd3
mike <mspiegel31@gmail.com>
2021-05-28 11:11:06 -0400

    adding more songs

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/config.h
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/config.h
@@ -5,7 +5,15 @@
     // #define STARTUP_SONG SONG(NO_SOUND)
 
    //  #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND) }
+   // more songs listed here: https://git.roxanne.dev/roxxers/qmk-custom-firmware/src/commit/b2175e18493a1e36d9762fb0e6397ba7bef20839/quantum/audio/song_list.h
    #undef MARIO_THEME
+   #undef MARIO_MUSHROOM
+   #undef COIN_SOUND
+   #undef ONE_UP_SOUND
+   #undef SONIC_RING
+   #undef ZELDA_TREASURE
+   #undef TERMINAL_SOUND
+
    #define MARIO_THEME \
     Q__NOTE(_E5), \
     H__NOTE(_E5), \
@@ -14,6 +22,62 @@
     H__NOTE(_E5), \
     W__NOTE(_G5), \
     Q__NOTE(_G4),
+
+
+   #define MARIO_MUSHROOM \
+      S__NOTE(_C5 ), \
+      S__NOTE(_G4 ), \
+      S__NOTE(_C5 ), \
+      S__NOTE(_E5 ), \
+      S__NOTE(_G5 ), \
+      S__NOTE(_C6 ), \
+      S__NOTE(_G5 ), \
+      S__NOTE(_GS4), \
+      S__NOTE(_C5 ), \
+      S__NOTE(_DS5), \
+      S__NOTE(_GS5), \
+      S__NOTE(_DS5), \
+      S__NOTE(_GS5), \
+      S__NOTE(_C6 ), \
+      S__NOTE(_DS6), \
+      S__NOTE(_GS6), \
+      S__NOTE(_DS6), \
+      S__NOTE(_AS4), \
+      S__NOTE(_D5 ), \
+      S__NOTE(_F5 ), \
+      S__NOTE(_AS5), \
+      S__NOTE(_D6 ), \
+      S__NOTE(_F6 ), \
+      S__NOTE(_AS6), \
+      S__NOTE(_F6 )
+
+   #define COIN_SOUND \
+    E__NOTE(_A5  ),   \
+    HD_NOTE(_E6  ),
+
+   #define ONE_UP_SOUND \
+    Q__NOTE(_E6  ),  \
+    Q__NOTE(_G6  ),  \
+    Q__NOTE(_E7  ),  \
+    Q__NOTE(_C7  ),  \
+    Q__NOTE(_D7  ),  \
+    Q__NOTE(_G7  ),
+
+   #define SONIC_RING \
+      E__NOTE(_E6),  \
+      E__NOTE(_G6),  \
+      HD_NOTE(_C7),
+
+   #define ZELDA_TREASURE \
+    Q__NOTE(_A4 ), \
+    Q__NOTE(_AS4), \
+    Q__NOTE(_B4 ), \
+    HD_NOTE(_C5 ), \
+
+   
+   #define TERMINAL_SOUND \
+      E__NOTE(_C5 )
+
 
 #endif
 
