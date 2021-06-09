1ae20c98ae855280787ce8535a5581ae66f91570
Mike Spiegel <mike@ripplescience.com>
2021-06-08 20:53:47 -0400

    moving symbols around

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -140,9 +140,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    _______, _______, KC_LT,   KC_GT,   KC_EQL,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_BSLS,
+    _______, _______, KC_LT,   KC_GT,   KC_EQL,  _______, _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
     _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_MPLY, KC_MUTE, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
   /* FN
@@ -197,8 +197,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
     MACOS_FORCE_QUIT, _______, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,   RGB_VAI, RGB_VAD, RESET ,
     _______,          _______, _______, _______, _______, _______, _______, QWERTY,  GAME_MODE, _______, _______, _______,
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______
+    _______,          _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MUTE,   _______, _______, _______,
+    _______,          _______, _______, _______, _______, _______, _______, _______, KC_MPRV,   KC_VOLD, KC_VOLU, KC_MNXT
 )
 
 };
