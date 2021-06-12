4715341c82c3f15cceadb823116a31c506232272
Mike Spiegel <mspiegel31@gmail.com>
2021-06-12 10:38:45 -0400

    moving brightness adjust to adjust layer

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_SPACE_FN] = LAYOUT_planck_grid(
-      C(KC_GRV), XXXXXXX, KC_BTN2,  KC_MS_U,   KC_BTN1,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_BRID, KC_BRIU,  XXXXXXX, XXXXXXX,
+      C(KC_GRV), XXXXXXX, KC_BTN2,  KC_MS_U,   KC_BTN1,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
       XXXXXXX,   XXXXXXX, KC_MS_L,  KC_MS_D,   KC_MS_R,  XXXXXXX,  XXXXXXX,  KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R, XXXXXXX,
       XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
       XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,  _______,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    MACOS_FORCE_QUIT, _______, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,   RGB_VAI, RGB_VAD, RESET ,
+    MACOS_FORCE_QUIT, _______, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, KC_BRID,   KC_BRIU, RGB_VAD, RESET ,
     _______,          _______, _______, _______, _______, _______, _______, QWERTY,  GAME_MODE, _______, _______, _______,
     _______,          _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
     _______,          _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______
