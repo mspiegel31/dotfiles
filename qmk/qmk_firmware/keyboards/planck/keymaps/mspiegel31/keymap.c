e183f635d9913372eb983dc2c865b8061fe0aa47
Mike Spiegel <mike@ripplescience.com>
2021-06-14 15:55:10 -0400

    moving mute away from , key

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     _______, _______, KC_LT,   KC_GT,   KC_EQL,  _______, _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
-    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_MPLY, KC_MUTE, _______, _______, _______,
+    _______, _______, _______, KC_PLUS, KC_MINS, _______, KC_MUTE, KC_MPLY, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
