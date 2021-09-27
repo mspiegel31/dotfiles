8c211a4f76e33194cad138fddd3a3ab86a8bf8e8
mspiegel31@gmail.com <mspiegel31@gmail.com>
2021-09-27 09:22:59 -0400

    flipping raise and lower

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    KC_LCTL, FN,      KC_LALT, KC_LGUI, LOWER,   KC_SPC,    KC_SPC, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+    KC_LCTL, FN,      KC_LALT, KC_LGUI, RAISE,   KC_SPC,    KC_SPC, LOWER,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
 ),
 
 
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    KC_LCTL, KC_1,    KC_2,    KC_3,    LOWER,   KC_SPC,    KC_SPC,   RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+    KC_LCTL, KC_1,    KC_2,    KC_3,    RAISE,   KC_SPC,    KC_SPC,   LOWER,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
 ),
 
 /* Lower
@@ -140,6 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
+
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -152,11 +153,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    _______, _______, KC_LT,   KC_GT,   KC_EQL,  _______, KC_AMPR   , KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
+    KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    _______, _______, KC_LT,   KC_GT,   KC_EQL,  _______, KC_AMPR, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
     _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
+
 
   /* FN
   * ,-----------------------------------------------------------------------------------.
