636e4fffb75963dc7c1000710b28ce733ee49a17
Mike Spiegel <mspiegel31@gmail.com>
2021-11-13 10:18:36 -0500

    adding fn keys under RH

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     TD(_CAPS_LOCK), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    KC_LCTL,        FN,      KC_LALT, KC_LGUI, RAISE,   KC_SPC,    KC_SPC, LOWER,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+    KC_LCTL,        FN,      KC_LALT, KC_LGUI, RAISE,   KC_SPC,    KC_SPC,   LOWER,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
 ),
 
 
@@ -138,8 +138,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    _______, _______, KC_LT,   KC_GT,   KC_EQL,  KC_PIPE, KC_AMPR, _______, _______, _______, _______, KC_BSLS,
-    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_UNDS, _______, _______, _______, _______,
+    _______, _______, KC_LT,   KC_GT,   KC_EQL,  KC_PIPE, KC_AMPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BSLS,
+    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
