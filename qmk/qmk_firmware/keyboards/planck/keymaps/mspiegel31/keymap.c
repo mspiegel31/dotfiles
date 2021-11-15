60276ed93673d8fdd2a41a9906ee795f6b6f31ba
Mike Spiegel <mspiegel31@gmail.com>
2021-11-15 11:28:26 -0500

    removing some unneeded F keys

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     _______, _______, KC_LT,   KC_GT,   KC_EQL,  KC_PIPE, KC_AMPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BSLS,
-    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_F5,   _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
