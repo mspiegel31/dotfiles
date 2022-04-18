7cacda767734738892af7f46a1d50ea6ee8fd74d
Mike Spiegel <mspiegel31@gmail.com>
2022-04-18 16:12:10 -0400

    adding zoom/unzoom

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -163,10 +163,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    _______, _______, KC_LT,   KC_GT,   KC_EQL,  KC_PIPE, KC_AMPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BSLS,
-    _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_F5,   _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_TILD,  KC_1,   KC_2,    KC_3,       KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    _______, _______, KC_LT,   KC_GT,      KC_EQL,  KC_PIPE, KC_AMPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BSLS,
+    _______, _______, _______, C(KC_MINS), C(KC_PLUS), _______, _______,    KC_F5,   _______, _______, _______, _______,
+    _______, _______, _______, _______,    _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Lower
