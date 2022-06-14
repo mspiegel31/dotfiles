84795b5d3fee2da44c175e09a09e08e5739994bf
Mike Spiegel <mspiegel@spoton.com>
2022-06-14 11:34:37 -0400

    formatting changes

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD,  KC_1,   KC_2,    KC_3,       KC_4,       KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     _______, _______, KC_LT,   KC_GT,      KC_EQL,     KC_PIPE, KC_AMPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BSLS,
-    _______, _______, _______, G(KC_MINS), G(KC_PLUS), _______, _______,    KC_F5,   _______, _______, _______, _______,
+    _______, _______, _______, G(KC_MINS), G(KC_PLUS), _______, _______, KC_F5,   _______, _______, _______, _______,
     _______, _______, _______, _______,    _______,    _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
@@ -181,10 +181,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-  KC_ESC,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  C(KC_GRV), _______,  _______, KC_COLN, KC_EQL,  KC_PIPE, KC_AMPR, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
-  _______,   C(KC_B),  KC_PERC, KC_UNDS, KC_MINS, KC_PLUS, _______, KC_LPRN, KC_RPRN, _______, _______, _______,
-  _______,   _______,  _______, _______, _______, KC_UNDS, KC_UNDS, _______, _______, _______, _______, _______
+  KC_ESC,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  C(KC_GRV), _______, _______, KC_COLN, KC_EQL,  KC_PIPE, KC_AMPR, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
+  _______,   C(KC_B), KC_PERC, KC_UNDS, KC_MINS, KC_PLUS, _______, KC_LPRN, KC_RPRN, _______, _______, _______,
+  _______,   _______, _______, _______, _______, KC_UNDS, KC_UNDS, _______, _______, _______, _______, _______
 ),
 
 
