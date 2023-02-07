de6be9ec3b129b6880822555ce58a2ee24b348e7
Mike Spiegel <mspiegel@spoton.com>
2023-02-07 09:11:03 -0500

    updating qmk keymap

--- qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/mspiegel31/keymap.c
@@ -183,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
   KC_ESC,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  C(KC_GRV), _______, _______, KC_COLN, KC_EQL,  KC_PIPE, KC_AMPR, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
+  C(KC_GRV), _______, _______, KC_GT,   KC_EQL,  KC_PIPE, KC_AMPR, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_BSLS,
   _______,   C(KC_B), KC_PERC, KC_UNDS, KC_MINS, KC_PLUS, _______, KC_LPRN, KC_RPRN, _______, _______, _______,
   _______,   _______, _______, _______, _______, KC_UNDS, KC_UNDS, _______, _______, _______, _______, _______
 ),
