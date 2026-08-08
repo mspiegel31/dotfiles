972cce838bff662642a4607b09611ba8d938190e
Michael Spiegel <mspiegel31@gmail.com>
2026-08-08 07:05:21 -0400

    preserve legacy Stow references

---
+++ docs/legacy/qtile-todo.md
@@ -0,0 +1,4 @@
+1. basic desired layouts
+1. floating terminal
+1. print qtile shortcuts to background?
+1. test with steam
---
+++ docs/legacy/stow-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update && sudo apt-get -y install stow
+
+#==============
+# Delete existing dot files and folders
+#==============
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+stow zsh -t $HOME
